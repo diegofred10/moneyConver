fe38586f3575d5387d20eda1d339b09a254dc981
diegofred10 <diegoa.gsm@gmail.com>
2023-01-21 18:41:13 +0100

    translated

--- moneyConver/src/components/Conver.vue
+++ moneyConver/src/components/Conver.vue
@@ -5,12 +5,12 @@
         <div>
           <div>
             <div>
-              <label for="cantidad">Ingrese Cantidad</label>
+              <label for="amount">Enter amount</label>
               <input
                 type="number"
-                id="cantidad"
-                placeholder="Ingrese cantidad.."
-                v-model="cantidad"
+                id="amount"
+                placeholder="Enter amount.."
+                v-model="amount"
                 v-on:keyup="onChange"
               />
             </div>
@@ -19,37 +19,37 @@
         <div>
           <div>
             <div>
-              <label for="origin">Origen</label>
+              <label for="origin">Origin</label>
               <select
                 id="origin"
                 v-model="origin"
                 v-on:change="onChange()"
               >
-                <option v-for="(moneda, index) in monedas" v-bind:key="index">
-                  {{ moneda }}
+                <option v-for="(currency, index) in currencys" v-bind:key="index">
+                  {{ currency }}
                 </option>
               </select>
             </div>
           </div>
           <div>
             <div>
-              <label for="exchange">Cambio</label>
+              <label for="exchange">Exchange</label>
               <select
                 id="exchange"
                 v-model="exchange"
                 v-on:change="onChange()"
               >
-                <option v-for="(moneda, index) in monedas" v-bind:key="index">
-                  {{ moneda }}
+                <option v-for="(currency, index) in currencys" v-bind:key="index">
+                  {{ currency }}
                 </option>
               </select>
             </div>
           </div>
         </div>
         <div>
-          <h3 v-if="cantidad > 0">
-            <span>{{ cantidad }} {{ origin }}</span>
-            <span> SON </span>
+          <h3 v-if="amount > 0">
+            <span>{{ amount }} {{ origin }}</span>
+            <span> = </span>
             <span>{{ getTotal(total) }} {{ exchange }}</span>
           </h3>
         </div>
@@ -61,8 +61,8 @@
   export default {
     data() {
       return {
-        monedas: ["USD", "EUR", "LEM", "YEN"],
-        cantidad: 0,
+        currencys: ["USD", "EUR", "LEM", "YEN"],
+        amount: 0,
         origin: "USD",
         exchange: "EUR",
         total: 0,
@@ -71,64 +71,64 @@
     methods: {
       onChange() {
         switch (this.origin) {
-          // Usuario selecciono que tiene dolares
+
           case "USD":
             if (this.exchange === "USD") {
-              this.total = this.cantidad;
+              this.total = this.amount;
             }
             if (this.exchange === "EUR") {
-              this.total = this.cantidad * 0.92;
+              this.total = this.amount * 0.92;
             }
             if (this.exchange === "LEM") {
-              this.total = this.cantidad * 24.67;
+              this.total = this.amount * 24.67;
             }
             if (this.exchange === "YEN") {
-              this.total = this.cantidad * 129.56;
+              this.total = this.amount * 129.56;
             }
             break;
-          // Usuario selecciono que tiene euros
+         
           case "EUR":
             if (this.exchange === "USD") {
-              this.total = this.cantidad * 1.09;
+              this.total = this.amount * 1.09;
             }
             if (this.exchange === "EUR") {
-              this.total = this.cantidad;
+              this.total = this.amount;
             }
             if (this.exchange === "LEM") {
-              this.total = this.cantidad * 26.83;
+              this.total = this.amount * 26.83;
             }
             if (this.exchange === "YEN") {
-              this.total = this.cantidad * 140.92;
+              this.total = this.amount * 140.92;
             }
             break;
-            // Usuario selecciono lempiras
+           
           case "LEM":
             if (this.exchange === "USD") {
-              this.total = this.cantidad * 0.041;
+              this.total = this.amount * 0.041;
             }
             if (this.exchange === "EUR") {
-              this.total = this.cantidad * 0.037;
+              this.total = this.amount * 0.037;
             }
             if (this.exchange === "LEM") {
-              this.total = this.cantidad;
+              this.total = this.amount;
             }
             if (this.exchange === "YEN") {
-              this.total = this.cantidad * 5.25;
+              this.total = this.amount * 5.25;
             }
             break;
-            // Usuario selecciono yenes
+          
             case "YEN":
             if (this.exchange === "USD") {
-              this.total = this.cantidad * 0.0077;
+              this.total = this.amount * 0.0077;
             }
             if (this.exchange === "EUR") {
-              this.total = this.cantidad * 0.0071;
+              this.total = this.amount * 0.0071;
             }
             if (this.exchange === "LEM") {
-              this.total = this.cantidad * 0.19;
+              this.total = this.amount * 0.19;
             }
             if (this.exchange === "YEN") {
-              this.total = this.cantidad;
+              this.total = this.amount;
             }
             break;
           default:
