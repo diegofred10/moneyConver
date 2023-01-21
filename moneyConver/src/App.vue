af2826f13b7a0fd40d9cfe5ec1f36447e5976b45
diegofred10 <diegoa.gsm@gmail.com>
2023-01-21 17:51:43 +0100

    convertidor

---
+++ moneyConver/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fondo">
+    <Navbar />
+    <Conver />
+  </div>
+</template>
+
+<script>
+import Conver from './components/Conver.vue';
+import Navbar from './components/Navbar.vue';
+export default {
+  name: "App",
+  components: {
+    Conver,
+    Navbar,
+  },
+};
+</script>
+
+<style>
+.fondo {
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
